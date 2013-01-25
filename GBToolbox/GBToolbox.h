4f971b97a260083656a8a11c39149549ee4fe5e7
Luka Mirosevic <luka@goonbee.com>
2013-01-25 15:58:43 +0100

    merged in some more methods from old string category i was using in VLC iRemote

--- GBToolbox/GBToolbox.h
+++ GBToolbox/GBToolbox.h
@@ -115,12 +115,19 @@ BOOL OddUInteger(NSUInteger number);
 
 @interface NSString (GBToolbox)
 
+#define GBStringUtilsLocalDNSSuffixes @{@"local", @"lan", @"group"}
+
 //check if string is integer
 -(BOOL)isInteger;
 
 //check if it contains a substring
 -(BOOL)containsSubstring:(NSString *)substring; //this is case sensitive
 -(BOOL)containsSubstring:(NSString *)substring caseSensitive:(BOOL)isCaseSensitive;
+
+-(NSString *)stringByDeletingDNSSuffix;
+
+-(BOOL)isIp;
+
 
 //best attempt to get int out of string
 -(int)attemptConversionToInt;
--- GBToolbox/GBToolbox.m
+++ GBToolbox/GBToolbox.m
@@ -285,6 +285,29 @@ BOOL OddUInteger(NSUInteger number) {
     return [numberString intValue];
 }
 
+-(NSString *)stringByDeletingDNSSuffix {
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"^(([0-9a-zA-Z]+[0-9a-zA-Z-]*[0-9a-zA-Z]+|[0-9a-zA-Z]+)[.][a-zA-Z]+)$" options:NSRegularExpressionCaseInsensitive error:NULL];
+    NSRange rangeOfFirstMatch = [regex rangeOfFirstMatchInString:self options:0 range:NSMakeRange(0, [self length])];
+    if (!NSEqualRanges(rangeOfFirstMatch, NSMakeRange(NSNotFound, 0))) {
+        NSRange dotRange = [self rangeOfString:@"."];
+        return [self substringToIndex:dotRange.location];
+    }
+    else {
+        return self;
+    }
+}
+
+-(BOOL)isIp {
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"^(([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])[.]([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])[.]([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])[.]([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]))$" options:0 error:NULL];
+    NSRange rangeOfFirstMatch = [regex rangeOfFirstMatchInString:self options:0 range:NSMakeRange(0, [self length])];
+    if (!NSEqualRanges(rangeOfFirstMatch, NSMakeRange(NSNotFound, 0))) {
+        return YES;
+    }
+    else {
+        return NO;
+    }
+}
+
 @end
 
 
