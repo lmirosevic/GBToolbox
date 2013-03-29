80fd7599074363c1b52eff5a3d691ea727510d42
Luka Mirosevic <luka@goonbee.com>
2013-03-29 17:24:40 +0100

    added simple import

--- GBToolbox/GBMacros_OSX.h
+++ GBToolbox/GBMacros_OSX.h
@@ -9,10 +9,12 @@
 #ifndef GBToolbox_GBMacros_OSX_h
 #define GBToolbox_GBMacros_OSX_h
 
+#import <Cocoa/Cocoa.h>
+
 //View controller instantiation
 static inline id InstantiateViewControllerWithXib(NSString *xibName) {
     NSString *className = [xibName stringByAppendingString:@"ViewController"];
-    return [[NSClassFromString(className) alloc] initWithNibName:className bundle:nil];
+    return [([NSClassFromString(className) alloc]) initWithNibName:className bundle:nil];
 }
 #define vc(xibName) InstantiateViewControllerWithXib(xibName)
 
