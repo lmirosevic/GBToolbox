0e6811dc192398c2d08bc7217fae924039f94e73
Luka Mirosevic <luka@goonbee.com>
2013-05-15 15:45:44 +0200

    added binary search to GBFastArray

--- GBToolbox/GBFastArray.h
+++ GBToolbox/GBFastArray.h
@@ -10,7 +10,21 @@
 
 extern NSString * const GBBadParameterException;
 
+extern NSUInteger const kGBSearchResultNotFound;
+
+typedef enum {
+    GBSearchResultMatch,
+    GBSearchResultLow,
+    GBSearchResultHigh,
+} GBSearchResult;
+
+//lambda for guiding binary search algo. there's no param for target item, but no need cuz it will get closed over from your calling context
+typedef GBSearchResult(^SearchLambda)(void *candidateItem);
+
 @interface GBFastArray : NSObject
+
+@property (assign, nonatomic, readonly) NSUInteger      count;
+@property (assign, nonatomic, readonly) BOOL            isEmpty;
 
 //Init
 -(id)initWithTypeSize:(NSUInteger)typeSize;
@@ -20,9 +34,13 @@ extern NSString * const GBBadParameterException;
 -(void)insertItem:(void *)itemAddress atIndex:(NSUInteger)index;
 -(void *)itemAtIndex:(NSUInteger)index;
 
+//Searching
+-(NSUInteger)binarySearchForIndexWithSearchLambda:(SearchLambda)searchLambda;
+-(NSUInteger)binarySearchForIndexWithLow:(NSUInteger)lowIndex high:(NSUInteger)highIndex searchLambda:(SearchLambda)searchLambda;
+
 //Size management
 -(NSUInteger)typeSize;
--(NSUInteger)currentArraySize;
+-(NSUInteger)currentAllocationSize;
 -(void)reallocToSize:(NSUInteger)newSize;
 -(void)setResizingFactor:(CGFloat)resizingFactor;
 
--- GBToolbox/GBFastArray.m
+++ GBToolbox/GBFastArray.m
@@ -10,17 +10,20 @@
 
 NSString * const GBBadParameterException = @"GBBadParameterException";
 
+NSUInteger const kGBSearchResultNotFound = NSUIntegerMax;
+
 static CGFloat const kDefaultResizingFactor = 1.5;
 static CGFloat const kDefaultInitialCapacity = 100;
 
 @implementation GBFastArray {
-    NSUInteger  _arraySize;
+    NSUInteger  _allocationSize;
+    NSUInteger  _lastElementIndex;
     NSUInteger  _typeSize;
     CGFloat     _resizingFactor;
     char        *_theArray;
 }
 
-#pragma mark - Memory
+#pragma mark - Public API: Init
 
 -(id)initWithTypeSize:(NSUInteger)typeSize {
     return [self initWithTypeSize:typeSize initialCapacity:kDefaultInitialCapacity resizingFactor:kDefaultResizingFactor];
@@ -30,11 +33,12 @@ static CGFloat const kDefaultInitialCapacity = 100;
     if (self = [super init]) {
         //State
         _typeSize = typeSize;
-        _arraySize = initialCapacity;
+        _allocationSize = initialCapacity;
+        _lastElementIndex = NSUIntegerMax;
         [self setResizingFactor:resizingFactor];
         
         //Alloc array
-        _theArray = malloc(_typeSize * _arraySize);
+        _theArray = malloc(_typeSize * _allocationSize);
     }
     
     return self;
@@ -45,12 +49,32 @@ static CGFloat const kDefaultInitialCapacity = 100;
     free(_theArray);
 }
 
-#pragma mark - Querying
+#pragma mark - Public API: Properties
+
+-(NSUInteger)count {
+    if (_lastElementIndex == NSUIntegerMax) {
+        return 0;
+    }
+    else {
+        return _lastElementIndex + 1;
+    }
+}
+
+-(BOOL)isEmpty {
+    return (_lastElementIndex == NSUIntegerMax);
+}
+
+#pragma mark - Public API: Querying
 
 -(void)insertItem:(void *)itemAddress atIndex:(NSUInteger)index {
     //if its too short, resize the array
-    if (index >= _arraySize) {
-        [self reallocToSize:(_arraySize * _resizingFactor)];
+    if (index >= _allocationSize) {
+        [self reallocToSize:(_allocationSize * _resizingFactor)];
+    }
+    
+    //if it stretches the lastElementIndex, set it
+    if (index > _lastElementIndex || _lastElementIndex == NSUIntegerMax) {
+        _lastElementIndex = index;
     }
     
     //put it in
@@ -61,19 +85,108 @@ static CGFloat const kDefaultInitialCapacity = 100;
     return _theArray + (index * _typeSize);
 }
 
-#pragma mark - Size management
+#pragma mark - Public API: Searching
+
+-(NSUInteger)binarySearchForIndexWithSearchLambda:(SearchLambda)searchLambda {
+    return [self binarySearchForIndexWithLow:0 high:_lastElementIndex searchLambda:searchLambda];
+}
+
+-(NSUInteger)binarySearchForIndexWithLow:(NSUInteger)lowIndex high:(NSUInteger)highIndex searchLambda:(SearchLambda)searchLambda {
+    //make sure its well formed
+    if (searchLambda && lowIndex <= highIndex) {
+        //prepare
+        NSUInteger midIndex;
+        GBSearchResult searchResult;
+        
+        //search while the subarray has at least 1 item
+        while (highIndex >= lowIndex) {
+            //calculate midpoint (as simple downrounded average)
+            midIndex = (highIndex+lowIndex)*0.5;
+            
+            //eval our search lambda
+            searchResult = searchLambda([self itemAtIndex:midIndex]);
+            
+            switch (searchResult) {
+                case GBSearchResultMatch: {
+                    return midIndex;
+                } break;
+                    
+                case GBSearchResultHigh: {
+                    highIndex = midIndex - 1;
+                } break;
+                    
+                case GBSearchResultLow: {
+                    lowIndex = midIndex + 1;
+                } break;
+            }
+        }
+    }
+    
+    //if we got here it means we didnt find anything, or the params were bad
+    return kGBSearchResultNotFound;
+    
+    //    // continue searching while [imin,imax] is not empty
+    //    while (imax >= imin)
+    //    {
+    //        /* calculate the midpoint for roughly equal partition */
+    //        int imid = midpoint(imin, imax);
+    //
+    //        // determine which subarray to search
+    //        if (A[imid] < key)
+    //            // change min index to search upper subarray
+    //            imin = imid + 1;
+    //        else if (A[imid] > key)
+    //            // change max index to search lower subarray
+    //            imax = imid - 1;
+    //        else
+    //            // key found at index imid
+    //            return imid;
+    //    }
+    //    // key not found
+    //    return KEY_NOT_FOUND;
+}
+
+//int binary_search(int A[], int key, int imin, int imax)
+//{
+//    // continue searching while [imin,imax] is not empty
+//    while (imax >= imin)
+//    {
+//        /* calculate the midpoint for roughly equal partition */
+//        int imid = midpoint(imin, imax);
+//
+//        // determine which subarray to search
+//        if (A[imid] < key)
+//            // change min index to search upper subarray
+//            imin = imid + 1;
+//        else if (A[imid] > key)
+//            // change max index to search lower subarray
+//            imax = imid - 1;
+//        else
+//            // key found at index imid
+//            return imid;
+//    }
+//    // key not found
+//    return KEY_NOT_FOUND;
+
+//}
+
+#pragma mark - Public API: Size management
 
 -(NSUInteger)typeSize {
     return _typeSize;
 }
 
--(NSUInteger)currentArraySize {
-    return _arraySize;
+-(NSUInteger)currentAllocationSize {
+    return _allocationSize;
 }
 
 -(void)reallocToSize:(NSUInteger)newSize {
-    _arraySize = newSize;
+    _allocationSize = newSize;
     _theArray = realloc(_theArray, (newSize * _typeSize));
+    
+    if (newSize < self.count) {
+        _lastElementIndex = newSize - 1;
+    }
 }
 
 -(void)setResizingFactor:(CGFloat)resizingFactor {
