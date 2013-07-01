94c4c88e65d4c8d11d571c4f211736ae25666eef
Luka Mirosevic <luka@goonbee.com>
2013-07-01 13:01:45 +0200

    Added some image resizing code

--- GBToolbox/UIImage+GBToolbox.h
+++ GBToolbox/UIImage+GBToolbox.h
@@ -5,12 +5,20 @@
 //  Created by Luka Mirosevic on 05/02/2013.
 //  Copyright (c) 2013 Luka Mirosevic. All rights reserved.
 //
+//
+//  Based on:
+//
+// UIImage+Resize.h
+// Created by Trevor Harmon on 8/5/09.
+// Free for personal or commercial use, with or without modification.
+// No warranty is expressed or implied.
 
 #import <UIKit/UIKit.h>
 
 @interface UIImage (GBToolbox)
 
-//crop to rect
 -(UIImage *)cropToRect:(CGRect)rect;
+-(UIImage *)resizedImage:(CGSize)newSize interpolationQuality:(CGInterpolationQuality)quality;
+-(UIImage *)resizedImageWithContentMode:(UIViewContentMode)contentMode bounds:(CGSize)bounds interpolationQuality:(CGInterpolationQuality)quality;
 
 @end
