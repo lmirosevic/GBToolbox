5a3f2caa6eeba546db88d1074cba96853aa2d55d
Luka Mirosevic <luka@goonbee.com>
2013-07-08 14:47:21 +0200

    Code comment

--- GBToolbox/GBTextView.h
+++ GBToolbox/GBTextView.h
@@ -5,7 +5,7 @@
 //  Created by Luka Mirosevic on 28/06/2013.
 //  Copyright (c) 2013 Goonbee. All rights reserved.
 //
-// Original code from SO: http://stackoverflow.com/a/1704469/399772
+//  Original code from SO: http://stackoverflow.com/a/1704469/399772
 
 #import <UIKit/UIKit.h>
 
